a2c2828440fa70cf13c90ac8f22b51a97f916af4
joshweinstock <jyweinstock@uchicago.edu>
2025-02-07 21:35:42 +0000

    rails_rps

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,47 @@
+class ZebraController < ApplicationController
+    def giraffe
+      moves = ["rock", "paper", "scissors"]
+      @opponent_move = moves.sample
+      if @opponent_move == "rock"
+        @outcome = "tied"
+      elsif @opponent_move == "paper"
+        @outcome = "lost"
+      else
+        @outcome = "won"
+      end
+      render({ :template => "game_templates/play_paper"})
+
+    end
+
+    def elephant
+      moves = ["rock", "paper", "scissors"]
+      @opponent_move = moves.sample
+      if @opponent_move == "rock"
+        @outcome = "won"
+      elsif @opponent_move == "paper"
+        @outcome = "tied"
+      else
+        @outcome = "lost"
+      end
+      render({ :template => "game_templates/play_paper"})
+    end
+
+    def lion
+      moves = ["rock", "paper", "scissors"]
+      @opponent_move = moves.sample
+      if @opponent_move == "rock"
+        @outcome = "lost"
+      elsif @opponent_move == "paper"
+        @outcome = "won"
+      else
+        @outcome = "tied"
+      end
+      render({ :template => "game_templates/play_scissors"})
+
+    end
+
+    def rules
+      render({ :template => "game_templates/home"})
+    end
+
+  end
